934950fb86a7d11afd78f30ad32755b5f3ca129b
ycp <511657675@qq.com>
2020-07-15 19:22:05 +0800

    Add id4 login
    Add id4 login

---
+++ Yan.MicroServices/Yan.MvcClient/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Yan.MvcClient.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+       
+        public IActionResult Index()
+        {
+            var name = User.Identity.Name;
+
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Login()
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public async Task< IActionResult>  Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+
+           return  RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ Yan.MicroServices/Yan.MvcClient/Startup.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityModel;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Yan.MvcClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            services.AddAuthentication(opions =>
+            {
+                opions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                opions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            }).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            {
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.Authority = "http://118.24.205.200:5100";
+                //options.Authority = "http://localhost:5100";
+                options.RequireHttpsMetadata = false;
+                options.ClientId = "Yan.MvcClient";
+                options.ClientSecret = "Yan.MvcClient";
+                options.SaveTokens = true;
+                options.ResponseType = "code";
+
+                options.Scope.Clear();
+                options.Scope.Add("system");
+                options.Scope.Add("article");
+                options.Scope.Add(OidcConstants.StandardScopes.OpenId);
+                options.Scope.Add(OidcConstants.StandardScopes.Profile);
+                options.Scope.Add(OidcConstants.StandardScopes.Address);
+                options.Scope.Add(OidcConstants.StandardScopes.Email);
+                options.Scope.Add(OidcConstants.StandardScopes.Phone);
+                // 与identity server的AllowOfflineAccess=true,对应。offline_access，指的是能否用refreshtoken重新申请令牌
+                options.Scope.Add(OidcConstants.StandardScopes.OfflineAccess);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
