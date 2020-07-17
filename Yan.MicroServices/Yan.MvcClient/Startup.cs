37c301f1a5257e310e475dd75559384c058796db
ycp <511657675@qq.com>
2020-07-17 19:15:04 +0800

    edit right menu

--- Yan.MicroServices/Yan.MvcClient/Startup.cs
+++ Yan.MicroServices/Yan.MvcClient/Startup.cs
@@ -26,7 +26,8 @@ namespace Yan.MvcClient
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation(); //cshtml 代码更改之后 不用重启服务 即可生效
 
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
             services.AddAuthentication(opions =>
