8287858ab0f91cda369d7cb3d9ba9d4d3f1fdfb4
xiaoyankuaipao <511657675@qq.com>
2021-10-12 20:50:48 +0800

    edit adminui2

---
+++ Yan.MicroServices/Yan.AdminUI2/ViewComponents/LeftMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Yan.AdminUI2.Models;
+
+namespace Yan.AdminUI2.ViewComponents
+{
+    [ViewComponent(Name = "LeftMenu")]
+    public class LeftMenuViewComponent:ViewComponent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IViewComponentResult> InvokeAsync(List<ElMenu> menus)
+        {
+            return View(menus);
+        }
+    }
+}
---
+++ Yan.MicroServices/Yan.AdminUI2/ViewComponents/LeftSubMenuViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Yan.AdminUI2.Models;
+
+namespace Yan.AdminUI2.ViewComponents
+{
+    [ViewComponent(Name = "LeftSubMenu")]
+    public class LeftSubMenuViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(ElMenu subMenu, List<ElMenu> menus)
+        {
+            return View(new ElSubMenu { SubMenu = subMenu, Menus = menus });
+        }
+    }
+}
